a7dd5591b9e20cf64bd7d5429fef8ccf7bd6868d
Ola Mobolaji <mobolajiola22@gmail.com>
2024-07-03 00:44:13 +0100

    tweak style

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 window.onload = function () {
-    // Update current time in UTC
-    function updateTime() {
-      const currentTime = new Date();
-      const utcTime = currentTime.toUTCString().split(" ")[4];
-      const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-      const utcDay = daysOfWeek[currentTime.getUTCDay()];
-      document.getElementById("currentTimeUTC").innerText = utcTime;
-      document.getElementById("currentDay").innerText = utcDay;
-    }
-  
-    updateTime();
-    setInterval(updateTime, 1000); // Update every second
-  };
-  
+  function updateTime() {
+    const currentTime = new Date();
+    const utcTime = currentTime.toUTCString().split(" ")[4];
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const utcDay = daysOfWeek[currentTime.getUTCDay()];
+    document.getElementById("currentTimeUTC").innerText = utcTime;
+    document.getElementById("currentDay").innerText = utcDay;
+  }
 
-//   const container = document.querySelector(".container");
-//   const profile = document.querySelector("#profile img");
-//   profile.addEventListener("mouseover", () => {
-//     container.style.background = randomBg();
-//   });
-
-//   function randomBg() {
-//     return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
-// }
-
-// };
+  updateTime();
+  setInterval(updateTime, 1000);
+};
